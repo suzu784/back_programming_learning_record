07fcff405beb8141f6bf9b634793e4bd94cc2699
suzu784 <s.suzuki784@hotmail.com>
2023-05-10 16:17:15 +0900

    ヘッダー・フッター・ビューの修正 #51

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="row justify-content-center">
-  <div class="col-md-2 col-lg-2">
+  <div class="col-md-2 col-lg-2 offset-lg-1">
     @include('users.user')
   </div>
-  <div class="col-md-10 col-lg-9">
+  <div class="col-md-10 col-lg-8">
     @include('users.tabs', ['hasRecords' => true, 'hasLikes' => false])
   </div>
 </div>
