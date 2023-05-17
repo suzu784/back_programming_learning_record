bdf5796d2c4d2726c50f1d35608958985f332bba
suzu784 <s.suzuki784@hotmail.com>
2023-05-17 11:50:59 +0900

    タグ機能を非同期にて実装 #67

---
+++ resources/js/components/TagInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <input
+            type="hidden"
+            name="tagName"
+            :value="tagsJson"
+            class="form-control"
+            id="tagName"
+        />
+        <Vue3TagsInput
+            :tags="tags"
+            placeholder="タグを入力してください。"
+            @on-tags-changed="(newTags) => (tags = newTags)"
+        />
+    </div>
+</template>
+
+<script>
+import Vue3TagsInput from "vue3-tags-input";
+export default {
+    components: {
+        Vue3TagsInput,
+    },
+    data() {
+        return {
+            tags: [],
+        };
+    },
+    computed: {
+        tagsJson() {
+            return this.tags;
+        },
+    },
+};
+</script>
