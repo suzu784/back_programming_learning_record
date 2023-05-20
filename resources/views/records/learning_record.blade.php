8b665ff91678beb79fdff99024ade769404d02c3
suzu784 <s.suzuki784@hotmail.com>
2023-05-20 12:06:00 +0900

    学習記録一覧機能の修正 #92

--- resources/views/records/learning_record.blade.php
+++ resources/views/records/learning_record.blade.php
@@ -3,8 +3,7 @@
     <hr>
     <table class="table table-borderless">
       <tbody>
-        @if($hasRecords === true && $hasLikes === false)
-        @foreach($records as $record)
+        @foreach ($hasRecords ? $records : $user->likes as $record)
         <tr class="h5">
           <td>
             {{ $record->learning_date }}
@@ -16,11 +15,11 @@
         <tr>
           <td class="h3"><a href="{{ route('records.show', ['record' => $record->id]) }}">
               {{ $record->title }}
-          </td>
+            </a></td>
         </tr>
         <tr>
           <td>
-            @if(Helper::isWithinMaxCharacters($record->body))
+            @if (Helper::isWithinMaxCharacters($record->body))
             {{ $record->body }}
             @else
             <p>{{ Helper::truncateText($record->body) }}</p>
@@ -29,33 +28,6 @@
           </td>
         </tr>
         @endforeach
-        @elseif($hasLikes === true && $hasRecords === false)
-        @foreach($user->likes as $record)
-        <tr class="h5">
-          <td>
-            {{ $record->learning_date }}
-          </td>
-        </tr>
-        <tr class="h5">
-          <td>学習時間{{ $record->duration }} 分</td>
-        </tr>
-        <tr>
-          <td class="h3"><a href="{{ route('records.show', ['record' => $record->id]) }}">
-              {{ $record->title }}
-          </td>
-        </tr>
-        <tr>
-          <td>
-            @if(Helper::isWithinMaxCharacters($record->body))
-            {{ $record->body }}
-            @else
-            <p>{{ Helper::truncateText($record->body) }}</p>
-            @endif
-            <hr>
-          </td>
-        </tr>
-        @endforeach
-        @endif
       </tbody>
     </table>
     {{ isset($records) ? $records->links() : $likes->links()}}
