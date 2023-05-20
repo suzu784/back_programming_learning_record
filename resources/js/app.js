4e4ae25b721168946d5280cc5ab4cbac8152baf8
suzu784 <s.suzuki784@hotmail.com>
2023-05-20 11:52:19 +0900

    学習記録詳細画面修正 #80

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,9 +9,10 @@ import RecordLike from "./components/RecordLike.vue";
 import CommentForm from "./components/CommentForm.vue";
 import TagInput from "./components/TagInput.vue";
 import StudyAnalytics from "./components/StudyAnalytics.vue";
+import PostContent from "./components/markdown/PostContent.vue";
 
 const goalFormElement = document.querySelector("#goal-form");
-if(goalFormElement) {
+if (goalFormElement) {
     const goalFormApp = createApp(GoalForm);
     goalFormApp.mount("#goal-form");
 }
@@ -33,14 +34,14 @@ const store = new Vuex.Store({
 });
 
 const textareaElement = document.querySelector("#text-area-modal");
-if(textareaElement) {
+if (textareaElement) {
     const textareaApp = createApp(Textarea);
     textareaApp.use(store);
     textareaApp.mount("#text-area-modal");
 }
 
 const likeButtonElement = document.querySelector("#like-button");
-if(likeButtonElement) {
+if (likeButtonElement) {
     const LikeButtonApp = async () => {
         const likeButtonApp = createApp({});
         likeButtonApp.component("record-like", RecordLike);
@@ -50,24 +51,34 @@ if(likeButtonElement) {
 }
 
 const commentFormElement = document.querySelector("#comment-form");
-if(commentFormElement) {
+if (commentFormElement) {
     const CommentFormApp = async () => {
         const commentFormApp = createApp({});
         commentFormApp.component("comment-form", CommentForm);
         commentFormApp.mount("#comment-form");
-    }
+    };
     CommentFormApp();
 }
 
 const tagInputElement = document.querySelector("#tag-input");
-if(tagInputElement) {
+if (tagInputElement) {
     const tagInputApp = createApp(TagInput);
     tagInputApp.mount("#tag-input");
 }
 
-const studyAnalytics = document.querySelector("#study-analytics");
-if(studyAnalytics) {
+const studyAnalyticsElement = document.querySelector("#study-analytics");
+if (studyAnalyticsElement) {
     const studyAnalyticsApp = createApp(StudyAnalytics);
     studyAnalyticsApp.use(VueChartkick);
     studyAnalyticsApp.mount("#study-analytics");
+}
+
+const postContentElement = document.querySelector("#post-content");
+if (postContentElement) {
+    const PostContentApp = async () => {
+        const postContentApp = createApp();
+        postContentApp.component("post-content", PostContent);
+        postContentApp.mount("#post-content");
+    };
+    PostContentApp();
 }
