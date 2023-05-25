c91fee59fddb6c9075df6c3d2e22263be81d69a7
suzu784 <s.suzuki784@hotmail.com>
2023-05-25 15:55:17 +0900

    テストデータの作成 #135

---
+++ database/factories/TemplateFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Template>
+ */
+class TemplateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => function () {
+                return User::factory()->create()->id;
+            },
+            'name' => 'test',
+            'content' => "echo 'Hello World!';",
+        ];
+    }
+}
